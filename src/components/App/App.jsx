f1c6b00930d42c169eba17dc20fb52663ccdd2bf
Emma Stout <emma@primeacademy.io>
2023-07-05 11:34:28 -0500

    add .map functions to display creature list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -20,6 +20,38 @@ function App() {
             {/* We can use JSON.stringify() to dump raw data into our DOM */}
             {JSON.stringify(creatureList)}
           </pre>
+          <p>
+            Getting one creature looks like:
+            {creatureList[0].name} is from {creatureList[0].origin}
+          </p>
+
+          {/* Loop over creatureList and for each creature, return
+              a new <p> tag */}
+          {/* .map always loops through an array and returns a
+              new array */}
+        <ul>
+          {creatureList.map(function (creature) {
+            console.log(creature);
+            // Keys let React uniquely identify each element
+            // We want to use something unique for a key - usually a database id
+            return (<li key={creature.name}>{creature.name} is from {creature.origin}.</li>);
+          }) }
+        </ul>
+
+        {/* VARIATION of .map: arrow function */}
+        <ul>
+          {creatureList.map((creature) => {
+            return (<li key={creature.name}>{creature.name} is from {creature.origin}</li>);
+          })}
+        </ul>
+
+        {/* VARIATION of .map: arrow function with implicit return and no parens around parameters */}
+        <ul>
+          {creatureList.map(creature => (<li key={creature.name}>{creature.name} is from {creature.origin}</li>))}
+        </ul>
+
+
+
         </div>
       </div>
     </>
